9a66f47c603efd95a51d0a5d3f05579e64cd0963
santanum73 <santanum73@gmail.com>
2021-10-24 00:25:05 +0530

    Update server.js

--- result/server.js
+++ result/server.js
@@ -71,7 +71,7 @@ function collectVotesFromResult(result) {
 
 app.use(cookieParser());
 // app.use(bodyParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded());
 app.use(bodyParser.json);
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(function(req, res, next) {
